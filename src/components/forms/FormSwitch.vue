bf1d886f9ceacb15544e1d6f41af058c7ef3ca1e
AxelS <contact@axelsimonet.fr>
2021-05-22 14:27:28 +0200

    feat(forms): fix v-model & add story

--- src/components/forms/FormSwitch.vue
+++ src/components/forms/FormSwitch.vue
@@ -2,8 +2,8 @@
   <label class="form-switch" :class="labelClasses">
     <slot name="false-value"></slot>
 
-    <input type="checkbox" v-bind="$attrs" v-on="$listeners" />
-    <span class="slider" :class="classes" :style="style"></span>
+    <input type="checkbox" v-model="computedValue" v-bind="$attrs" ref="input" />
+    <span class="slider" :class="classes" :style="style" ref="slider"></span>
 
     <slot></slot>
   </label>
@@ -13,7 +13,13 @@
 export default {
   name: 'AxFormSwitch',
   inheritAttrs: false,
+  data() {
+    return {
+      localValue: this.value,
+    };
+  },
   props: {
+    value: [String, Number, Boolean, Function, Object, Array],
     labelClasses: {
       type: String,
       default: '',
@@ -32,6 +38,11 @@ export default {
       default: '#e92626',
     },
   },
+  watch: {
+    value(val) {
+      this.localValue = val;
+    },
+  },
   computed: {
     classes() {
       return {
@@ -44,6 +55,15 @@ export default {
       return {
         '--form-switch-active-color': this.color,
       };
+    },
+    computedValue: {
+      get() {
+        return this.localValue;
+      },
+      set(val) {
+        this.localValue = val;
+        this.$emit('input', val);
+      },
     },
   },
 };
