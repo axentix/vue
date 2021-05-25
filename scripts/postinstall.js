cfa63fb6790dec06b39dbd321caac0bc55dbfcd8
AxelS <contact@axelsimonet.fr>
2021-05-25 16:17:51 +0200

    feat: switch to vitejs /  improve vue detection

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,21 +4,21 @@ const pkg = require('../package.json');
 const vue = require('vue');
 
 const version = vue.version;
-const isVue3 = +version.split('.')[0] === 3;
+const isVue2 = +version.split('.')[0] === 2;
 
 let v;
-if (isVue3) {
-  console.log('[Vue Axentix] Detected Vue 3, switch main entry to Vue 3');
-  v = '3';
-} else {
+if (isVue2) {
   console.log('[Vue Axentix] Detected Vue 2, switch main entry to Vue 2');
   v = '2';
+} else {
+  console.log('[Vue Axentix] Detected Vue 3, switch main entry to Vue 3');
+  v = '3';
 }
 
 pkg.main = `dist/vue${v}/vue-axentix.cjs.js`;
-pkg.module = `dist/vue${v}/vue-axentix.esm.js`;
-pkg.browser = `dist/vue${v}/vue-axentix.esm.js`;
-pkg.unpkg = `dist/vue${v}/vue-axentix.min.js`;
+pkg.module = `dist/vue${v}/vue-axentix.es.js`;
+pkg.browser = `dist/vue${v}/vue-axentix.es.js`;
+pkg.unpkg = `dist/vue${v}/vue-axentix.iife.js`;
 
 fs.writeFileSync(path.resolve(__dirname, '../package.json'), JSON.stringify(pkg, null, 2), {
   encoding: 'utf-8',
---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite';
+import { createVuePlugin } from 'vite-plugin-vue2';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { isVue3 } from 'vue-demi';
+
+const plugins = [];
+let outDir = 'dist/';
+
+if (isVue3) {
+  plugins.push(vue());
+  outDir += 'vue3/';
+} else {
+  plugins.push(createVuePlugin());
+  outDir += 'vue2/';
+}
+
+export default defineConfig({
+  plugins,
+  server: {
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      '@': `${path.resolve(__dirname, 'src')}`,
+    },
+  },
+  build: {
+    outDir,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'VueAxentix',
+      formats: ['cjs', 'es', 'iife'],
+      fileName: 'vue-axentix',
+    },
+    rollupOptions: {
+      external: ['vue', 'vue-demi'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          'vue-demi': 'VueDemi',
+        },
+      },
+    },
+  },
+});
