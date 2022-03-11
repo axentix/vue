ac623a38e17ea7e41b246979738408e9bec2519b
AxelS <contact@axelsimonet.fr>
2022-03-11 17:00:55 +0100

    feat: switch to vue 3

---
+++ dev/vue2/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+import VueAxentix from '@/index';
+
+import 'axentix/dist/axentix.min.css';
+
+Vue.config.productionTip = false;
+
+Vue.use(VueAxentix);
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
---
+++ scripts/swap.js
@@ -0,0 +1,40 @@
+const { rename, existsSync } = require('fs');
+const { join } = require('path');
+
+const Vue2 = join(__dirname, '../node_modules/vue2');
+const DefaultVue = join(__dirname, '../node_modules/vue');
+const Vue3 = join(__dirname, '../node_modules/vue3');
+
+const version = Number(process.argv[2]) || 3;
+
+const move = (from, to) => {
+  if (!existsSync(from)) return;
+
+  rename(from, to, (err) => {
+    if (err) console.error(`[Vue Axentix] ${err}`);
+  });
+};
+
+const useVueVersion = (version) => {
+  if (!existsSync(DefaultVue)) {
+    if (version === 2 && existsSync(Vue3)) {
+      move(Vue3, DefaultVue);
+    } else {
+      move(Vue2, DefaultVue);
+    }
+
+    console.log(`[Vue Axentix] Swapped to Vue ${version}`);
+  }
+
+  if (version === 3 && existsSync(Vue3)) {
+    move(DefaultVue, Vue2);
+    move(Vue3, DefaultVue);
+  } else if (version === 2 && existsSync(Vue2)) {
+    move(DefaultVue, Vue3);
+    move(Vue2, DefaultVue);
+  } else {
+    console.log(`[Vue Axentix] Vue ${version} is already in use`);
+  }
+};
+
+useVueVersion(version);
